97b908e252c19a74222abd71a9d8b05a2a7f50d6
aleksyprok <alex.prokopyszyn@ukaea.uk>
2024-03-05 14:28:03 +0000

    Added shell scripts

--- shell_scripts/compile.sh
+++ shell_scripts/compile.sh
@@ -28,5 +28,81 @@ blocksPerGrid=256
 timax="1.0_gpu"
 UNBOR=1000
 dt0="1.0e-06_gpu"
-BPLASMA=0
-ncoil=16
+
+# Make a load of lists to store the values of the parameters we are varying
+rcoils=()
+ncoils=()
+bripples=()
+toroidal_modes=()
+coil_sets=()
+currents=()
+responses=()
+gain_values=()
+bscales=()
+bplasmas=()
+
+# Axisymmetric simulations
+rcoil=0
+ncoil=0
+bripple=0
+toroidal_mode=0
+coil_set="none"
+current=0
+response=0
+phase=0
+gain_value=0
+bscale=0
+bplasma=0
+rwm_control=0
+rmp=0
+rcoils+=("$rcoil")
+ncoils+=("$ncoil")
+bripples+=("$bripple")
+toroidal_modes+=("$toroidal_mode")
+coil_sets+=("$coil_set")
+currents+=("$current")
+responses+=("$response")
+phases+=("$phase")
+gain_values+=("$gain_value")
+bscales+=("$bscale")
+bplasmas+=("$bplasma")
+rwm_contrls+=("$rwm_control")
+rmps+=("$rmp")
+
+# Ripple simulations
+bripple=1
+toroidal_mode=0
+coil_set="none"
+current=0
+response=0
+phase=0
+gain_value=0
+bscale=0
+bplasma=0
+rwm_control=0
+rmp=0
+ncoils_unique=(12 16 18)
+rcoils_unique=("7.0" "7.25" "7.5" "7.75" "8.0" "8.25" "8.5" "8.75" "9.0")
+for ncoil in "${ncoils_unique[@]}"; do
+    for rcoil in "${rcoils_unique[@]}"; do
+        ncoils+=("$ncoil")
+        rcoils+=("$rcoil")
+        bripples+=("$bripple")
+        toroidal_modes+=("$toroidal_mode")
+        coil_sets+=("$coil_set")
+        currents+=("$current")
+        responses+=("$response")
+        phases+=("$phase")
+        gain_values+=("$gain_value")
+        bscales+=("$bscale")
+        bplasmas+=("$bplasma")
+        rwm_contrls+=("$rwm_control")
+        rmps+=("$rmp")
+    done
+done
+
+# RMP simulations
+
+# RMP special phases simulations
+
+# RWM control simulations
