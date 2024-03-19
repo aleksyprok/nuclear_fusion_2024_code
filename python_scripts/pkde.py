04463b35b4276aa8f47445ccaea24bc84030c2eb
aleksyprok <alex.prokopyszyn@ukaea.uk>
2024-03-19 16:52:22 +0000

    Added periodic kde functions with tests.

--- python_scripts/pkde.py
+++ python_scripts/pkde.py
@@ -4,6 +4,8 @@ over 1D and 2D domains. With periodic boundary conditions.
 """
 
 import numpy as np
+from scipy import interpolate
+from KDEpy import FFTKDE
 
 def extend_coords(x, y, x_min, x_max, y_min, y_max, weights):
     """
@@ -63,3 +65,128 @@ def extend_coords(x, y, x_min, x_max, y_min, y_max, weights):
     weights_extended = weights_extended[reduced_indices]
 
     return x_extended, y_extended, weights_extended
+def periodic_kde_1d(x, x_min, x_max, weights, hx,
+                    kernel='gaussian',
+                    num_grid_points=10**3):
+    """
+    Calculates a 1D kernel density estimate using FFTs with periodic
+    boundary conditions.
+
+    Parameters
+    ----------
+    x : array
+        1D array of x coordinates
+    x_min : float
+        Minimum x coordinate
+    x_max : float
+        Maximum x coordinate
+    weights : array
+        1D array of weights
+    hx : float
+        Bandwidth in metres
+    kernel : str
+        KDE kernel
+    num_grid_points : int
+        Number of grid points
+
+    Returns
+    -------
+    pdf_fun : function
+        KDE estimate.
+    """
+
+    # Extending coordiantes
+    period_x = x_max - x_min
+    x_norm = np.concatenate((x - period_x, x, x + period_x))
+    weights_norm = np.concatenate((weights, weights, weights))
+    delta_x = period_x / 2
+    reduced_indices = np.where(
+        (x_min - delta_x <= x_norm) *
+                           (x_norm <= x_max + delta_x))
+    x_norm = x_norm[reduced_indices]
+    weights_norm = weights_norm[reduced_indices]
+    # Double number of grid points to account for extention
+    num_grid_points *= 2
+
+    # Stretching coordiantes
+    x_norm = x_norm / hx
+
+    # Calculating extended and stretched KDE
+    fft_kde = FFTKDE(kernel = kernel, bw = 1)
+    grid_norm_x, pdf = \
+        fft_kde.fit(x_norm, weights = weights_norm).evaluate(num_grid_points)
+    pdf *= np.sum(weights_norm) / np.sum(weights) / hx # Renormalise
+
+    # Evaluating extended and stretched KDE on an unstretched grid
+    grid_x = hx * grid_norm_x
+    pdf_fun = interpolate.UnivariateSpline(grid_x, pdf,
+                                           s=0, ext='raise')
+
+    return pdf_fun
+
+def periodic_kde_2d(x, y, x_min, x_max, y_min, y_max, weights,
+                    hx, hy,
+                    kernel = 'gaussian',
+                    num_grid_points = 10**3):
+    """
+    Calculates a 2D kernel density estimate using FFTs with periodic
+    boundary conditions.
+
+    Parameters
+    ----------
+    x : array
+        1D array of x coordinates
+    y : array
+        1D array of y coordinates
+    x_min : float
+        Minimum x coordinate
+    x_max : float
+        Maximum x coordinate
+    y_min : float
+        Minimum y coordinate
+    y_max : float
+        Maximum y coordinate
+    weights : array
+        1D array of weights
+    hx : float
+        Bandwidth in metres
+    hy : float
+        Bandwidth in metres
+    kernel : str
+        KDE kernel
+    num_grid_points : int
+        Number of grid points
+
+    Returns
+    -------
+    pdf_fun : array
+        2D function  which interpolates the FFTKDE
+    """
+
+    # Extending coordiantes
+    x_norm, y_norm, weights_norm = extend_coords(x, y, x_min, x_max, y_min, \
+                                                 y_max, weights)
+    # Double number of grid points to account for extention
+    num_grid_points *= 2
+
+    # Stretching coordiantes
+    x_norm = x_norm / hx
+    y_norm = y_norm / hy
+
+    # Calculating extended and stretched KDE
+    fft_kde = FFTKDE(kernel=kernel, bw=1)
+    coords_norm = np.vstack([x_norm, y_norm]).T
+    grid_norm, pdf = \
+        fft_kde.fit(coords_norm, weights = weights_norm).evaluate(num_grid_points)
+    grid_norm_x, grid_norm_y = np.unique(grid_norm[:, 0]), np.unique(grid_norm[:, 1])
+    pdf = pdf.reshape(num_grid_points, num_grid_points) \
+        * np.sum(weights_norm) / np.sum(weights) / (hx * hy) # Renormalise
+
+    # Evaluating extended and stretched KDE on an unstretched grid
+    # Apply optional scale factor)
+    grid_x = grid_norm_x * hx
+    grid_y = grid_norm_y * hy
+    pdf_fun = interpolate.RectBivariateSpline(grid_x, grid_y, pdf)
+    # pdf_fun = interpolate.interp2d(grid_x, grid_y, pdf, \
+    #                                kind = 'cubic', bounds_error = True)
+    return pdf_fun
--- tests/test_pkde.py
+++ tests/test_pkde.py
@@ -1,8 +1,13 @@
 """
 Module to test the pkde module.
 """
+import os
+import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 from python_scripts import pkde
+import warnings
+warnings.filterwarnings("error", category=DeprecationWarning)
 
 def test_extend_coords():
     """
@@ -20,7 +25,7 @@ def test_extend_coords():
     weights = np.array([1, 2, 3])
     x_extended, y_extended, weights_extended = \
         pkde.extend_coords(x, y, x_min, x_max, y_min, y_max, weights)
-    
+
     # x_test = np.array([-3, -2, -1,
     #                    -3, -2, -1,
     #                    -3, -2, -1,
@@ -84,3 +89,102 @@ def test_extend_coords():
     assert np.array_equal(x_extended, x_test)
     assert np.array_equal(y_extended, y_test)
     assert np.array_equal(weights_extended, weights_test)
+
+def test_periodic_kde_1d():
+    """
+    Test case for the periodic_kde_1d function.
+
+    This test case verifies that the periodic_kde_1d function returns the expected
+    KDE estimate for a given set of coordinates.
+    """
+    x = np.array([1, 2, 3])
+    x_min = 0
+    x_max = 4
+    weights = np.array([1, 2, 3])
+    hx = 0.3
+    kernel = 'gaussian'
+    num_grid_points = 10**3
+    pdf_fun = pkde.periodic_kde_1d(x, x_min, x_max, weights, hx,
+                                   kernel = kernel,
+                                   num_grid_points = num_grid_points)
+
+    # Calcualte KDE using scipy, not FFTKDE
+    x = np.array([-3, -2, -1, +1, +2, +3, +5, +6, +7])
+    sigma_x = np.std(x)
+    weights = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
+    kde_scipy = stats.gaussian_kde(x,
+                                   bw_method=hx/sigma_x,
+                                   weights=weights)
+
+    # Plot the KDE estimate
+    grid_x = np.linspace(x_min, x_max, num_grid_points)
+    fig, ax = plt.subplots()
+    ax.plot(grid_x, pdf_fun(grid_x))
+    ax.plot(grid_x, 3 * kde_scipy(grid_x))
+    ax.set_xlabel('x')
+    ax.set_ylabel('PDF')
+    ax.set_title('KDE estimate')
+    ax.legend(['Periodic KDE', 'Scipy KDE'])
+    repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    output_dir = os.path.join(repo_path, "tests", "output_plots")
+    outputpath = os.path.join(output_dir, 'periodic_kde_1d.png')
+    fig.savefig(outputpath)
+
+    # Check integral of the PDE is 1 within a 10^-4 tolerance
+    assert np.isclose(np.trapz(pdf_fun(grid_x), grid_x),
+                      1,
+                      atol=1e-4)
+    # Check the integral of scipy KDE is 1 within a 10^-4 tolerance
+    assert np.isclose(np.trapz(3 * kde_scipy(grid_x), grid_x),
+                      1,
+                      atol=1e-4)
+    # Check the KDE estimate is within 10% of the scipy KDE
+    assert np.isclose(np.trapz(pdf_fun(grid_x), grid_x),
+                      np.trapz(3 * kde_scipy(grid_x), grid_x),
+                      atol=0.1)
+
+def test_periodic_kde_2d():
+    """
+    Test case for the periodic_kde_2d function.
+
+    This test case verifies that the periodic_kde_2d function returns the expected
+    KDE estimate for a given set of coordinates.
+    """
+    repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    output_dir = os.path.join(repo_path, "tests", "output_plots")
+
+    x = np.array([1, 2, 3])
+    y = np.array([2, 5.6, 2])
+    x_min = 0
+    x_max = 4
+    y_min = 1
+    y_max = 5.7
+    weights = np.array([1, 2, 3])
+    hx = 0.3
+    hy = 0.4
+    kernel = 'gaussian'
+    num_grid_points = 10**3
+    pdf_fun = pkde.periodic_kde_2d(x, y, x_min, x_max, y_min, y_max, weights,
+                                   hx, hy,
+                                   kernel=kernel,
+                                   num_grid_points=num_grid_points)
+    grid_x = np.linspace(x_min, x_max, num_grid_points)
+    grid_y = np.linspace(y_min, y_max, num_grid_points)
+    pdf_fft = pdf_fun(grid_x, grid_y)
+
+    # Plot the KDE estimate image
+    fig, ax = plt.subplots()
+    im = ax.imshow(pdf_fft.T, extent=[x_min, x_max, y_min, y_max],
+                   origin='lower')
+    ax.set_title('FFT KDE estimate')
+    fig.colorbar(im)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.scatter(x, y)
+    outputpath = os.path.join(output_dir, 'periodic_kde_2d.png')
+    fig.savefig(outputpath)
+
+    # Check integral is equal to 1
+    assert np.isclose(np.trapz(np.trapz(pdf_fft, grid_x, axis=0), grid_y, axis =0),
+                      1,
+                      atol=1e-4)
