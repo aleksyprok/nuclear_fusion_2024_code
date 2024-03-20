decbeabc4b0a4c5feba4ea906bcec75fb27fbbf5
aleksyprok <alex.prokopyszyn@ukaea.uk>
2024-03-20 12:12:27 +0000

    Updated the docstring.

--- python_scripts/run.py
+++ python_scripts/run.py
@@ -1,6 +1,15 @@
 """
 This module contains routines for updating a Run object which stores information
 about a LOCUST run.
+
+The run object contain three nested classes, log, stopped and grid.
+The log class contains information obtained by reading the LOG*.out file.
+The stopped class contains information obtained by reading the FINAL_STATE*.dat file
+and only stores information about particles that have hit the wall.
+The grid class contains information obtained by reading the wall 2D wall files, e.g.
+input_data/SPP-001_wall.dat.
+Note that the log class and grid classes are standalone classes, but many of the methods in the
+stopped module require the log and grid classes.
 """
 from typing import Optional
 from python_scripts import fstate, log
@@ -17,6 +26,7 @@ class Run:
         self.tag = tag
         self.log_path = self.dir_path + f'/LOG_{self.tag}.out'
         self.fstate_path = self.dir_path + f'/FINAL_STATE_{self.tag}.dat'
+        self.einj = None
 
     def update_log(self):
         """
