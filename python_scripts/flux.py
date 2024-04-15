ddeae4fb9589b203ad4062b8e0b079dc92048c9c
aleksyprok <alex.prokopyszyn@ukaea.uk>
2024-04-15 21:56:22 +0100

    Changed flux.py to work with bootstrap.py better

--- python_scripts/flux.py
+++ python_scripts/flux.py
@@ -13,9 +13,15 @@ class Flux:
     """
     This class contains routines for calculating the energy flux on the PFCs
     """
-    def __init__(self, num_grid_points = 1000):
-        self.num_grid_points = num_grid_points
-        self.s_theta: Optional[np.ndarray] = None
+    def __init__(self,
+                 num_grid_points_1d=10**4,
+                 num_grid_points_2d=10**3,
+                 num_bootstraps=128):
+        self.num_grid_points_1d = num_grid_points_1d
+        self.num_grid_points_2d = num_grid_points_2d
+        self.num_bootstraps = num_bootstraps
+        self.s_theta_1d: Optional[np.ndarray] = None
+        self.s_theta_2d: Optional[np.ndarray] = None
         self.s_phi: Optional[np.ndarray] = None
         self.total_energy: Optional[float] = None # Store the total energy flux in MW on the PFCs
         self.h_phi: Optional[float] = None
@@ -28,6 +34,9 @@ class Flux:
         self.h_theta_2d_array: Optional[np.ndarray] = None
         self.amise_1d: Optional[np.ndarray] = None
         self.amise_2d: Optional[np.ndarray] = np.array([[None]])
+        self.conf_band_1d: Optional[float] = None
+        self.conf_band_2d: Optional[float] = None
+        self.conf_band_total: Optional[float] = None
 
 def calc_s_theta_s_phi(run):
     """
@@ -36,12 +45,19 @@ def calc_s_theta_s_phi(run):
     """
     run.flux.s_phi = np.linspace(run.wall.s_phi_min,
                                  run.wall.s_phi_max,
-                                 run.flux.num_grid_points)
-    run.flux.s_theta = np.linspace(run.wall.s_theta_min,
-                                   run.wall.s_theta_max,
-                                   run.flux.num_grid_points)
+                                 run.flux.num_grid_points_2d)
+    run.flux.s_theta_1d = np.linspace(run.wall.s_theta_min,
+                                      run.wall.s_theta_max,
+                                      run.flux.num_grid_points_1d)
+    run.flux.s_theta_2d = np.linspace(run.wall.s_theta_min,
+                                      run.wall.s_theta_max,
+                                      run.flux.num_grid_points_2d)
 
-def calc_total_energy(run):
+def calc_total_energy(run,
+                      stopped_energy=None,
+                      all_energy0=None,
+                      stopped_weight=None,
+                      all_weight=None):
     """
     Calculate the total energy flux on the Plasma-Facing Components (PFCs) in megawatts (MW).
 
@@ -62,14 +78,21 @@ def calc_total_energy(run):
         float: The total energy flux on the PFCs in MW. If the requirements are not 
             met, the behavior of this method is undefined and may result in errors.
     """
-    energy0 = run.markers.all.vr0**2 + run.markers.all.vphi0**2 + run.markers.all.vz0**2
-    denominator = np.sum(energy0 * run.markers.all.weight)
-    stopped_energy = run.markers.stopped.vr**2 + run.markers.stopped.vphi**2 \
-                    + run.markers.stopped.vz**2
-    run.flux.total_energy = run.log.pinj * np.sum(stopped_energy * run.markers.stopped.weight) \
-                          / denominator
+    if stopped_energy is None:
+        stopped_energy = run.markers.stopped.energy
+    if stopped_weight is None:
+        stopped_weight = run.markers.stopped.weight
+    if all_energy0 is None:
+        all_energy0 = run.markers.all.energy0
+    if all_weight is None:
+        all_weight = run.markers.all.weight
+    return run.log.pinj * np.sum(stopped_energy * stopped_weight) \
+                        / np.sum(all_energy0 * all_weight)
 
 def calc_energy_flux_1d(run,
+                        stopped_s_theta=None,
+                        stopped_energy=None,
+                        stopped_weight=None,
                         h_theta_1d=None):
     """
     Calculate the energy flux on the PFCs using the distribution of the
@@ -90,33 +113,41 @@ def calc_energy_flux_1d(run,
             in MW/m^2.
 
     """
-
+    if stopped_s_theta is None:
+        stopped_s_theta = run.markers.stopped.s_theta
+    if stopped_energy is None:
+        stopped_energy = run.markers.stopped.energy
+    if stopped_weight is None:
+        stopped_weight = run.markers.stopped.weight
     if h_theta_1d is None:
         h_theta_1d = run.flux.h_theta_1d
-    kde_fun = pkde.periodic_kde_1d(run.markers.stopped.s_theta,
+    kde_fun = pkde.periodic_kde_1d(stopped_s_theta,
                                    run.wall.s_theta_min,
                                    run.wall.s_theta_max,
-                                   run.markers.stopped.weight,
+                                   stopped_weight * stopped_energy,
                                    h_theta_1d,
                                    kernel='gaussian',
-                                   num_grid_points=run.flux.num_grid_points)
-    kde_array = kde_fun(run.flux.s_theta)
+                                   num_grid_points=run.flux.num_grid_points_1d)
+    kde_array = kde_fun(run.flux.s_theta_1d)
 
     # Calculate bias corrected KDE
     asymptotic_bias = \
-        pkde.calc_asymptotic_bias_1d(kde_fun, run.flux.s_theta, h_theta_1d)
+        pkde.calc_asymptotic_bias_1d(kde_fun, run.flux.s_theta_1d, h_theta_1d)
     kde_array = kde_array - asymptotic_bias
 
     # Calculate unstretched KDE
-    kde_array *= run.wall.scale_factor_1d(run.flux.s_theta)
+    kde_array *= run.wall.scale_factor_1d(run.flux.s_theta_1d)
 
     # Calculate energy flux
     energy_flux = kde_array * run.flux.total_energy
 
-    run.flux.energy_1d = energy_flux
-    run.flux.h_theta_1d = h_theta_1d
+    return energy_flux
 
 def calc_energy_flux_2d(run,
+                        stopped_s_phi=None,
+                        stopped_s_theta=None,
+                        stopped_energy=None,
+                        stopped_weight=None,
                         h_phi=None,
                         h_theta_2d=None):
     """
@@ -139,27 +170,33 @@ def calc_energy_flux_2d(run,
 
     """
 
+    if stopped_s_phi is None:
+        stopped_s_phi = run.markers.stopped.s_phi
+    if stopped_s_theta is None:
+        stopped_s_theta = run.markers.stopped.s_theta
+    if stopped_energy is None:
+        stopped_energy = run.markers.stopped.energy
+    if stopped_weight is None:
+        stopped_weight = run.markers.stopped.weight
     if h_theta_2d is None:
         h_theta_2d = run.flux.h_theta_2d
     if h_phi is None:
         h_phi = run.flux.h_phi
 
-    kde_fun = pkde.periodic_kde_2d(run.markers.stopped.s_phi,
-                                   run.markers.stopped.s_theta,
+    kde_fun = pkde.periodic_kde_2d(stopped_s_phi,
+                                   stopped_s_theta,
                                    run.wall.s_phi_min,
                                    run.wall.s_phi_max,
                                    run.wall.s_theta_min,
                                    run.wall.s_theta_max,
-                                   run.markers.stopped.weight,
+                                   stopped_weight * stopped_energy,
                                    h_phi,
                                    h_theta_2d,
-                                   num_grid_points=run.flux.num_grid_points)
-    kde_array = kde_fun(run.flux.s_phi, run.flux.s_theta)
+                                   num_grid_points=run.flux.num_grid_points_2d)
+    kde_array = kde_fun(run.flux.s_phi, run.flux.s_theta_2d)
     # Calculate energy flux
     energy_flux = kde_array * run.flux.total_energy
-    run.flux.energy_2d = energy_flux
-    run.flux.h_theta_2d = h_theta_2d
-    run.flux.h_phi = h_phi
+    return energy_flux
 
 def calc_optimum_bandwidth_1d(run, h_theta_1d_array=None):
     """
@@ -185,10 +222,10 @@ def calc_optimum_bandwidth_1d(run, h_theta_1d_array=None):
     if h_theta_1d_array is None:
         h_theta_1d_array = run.flux.h_theta_1d_array
     amise_array = pkde.calc_amise_1d_array(run.markers.stopped.s_theta,
-                                           run.flux.s_theta,
-                                           run.markers.stopped.weight,
+                                           run.flux.s_theta_1d,
+                                           run.markers.stopped.weight * run.markers.stopped.energy,
                                            h_theta_1d_array,
-                                           num_grid_points=run.flux.num_grid_points)
+                                           num_grid_points=run.flux.num_grid_points_1d)
     h_theta_1d = h_theta_1d_array[np.argmin(amise_array)]
     run.flux.amise_1d = amise_array
     run.flux.h_theta_1d = h_theta_1d
@@ -230,11 +267,11 @@ def calc_optimum_bandwidth_2d(run, h_phi_array=None, h_theta_2d_array=None):
     amise_array = pkde.calc_amise_2d_array(run.markers.stopped.s_phi,
                                            run.markers.stopped.s_theta,
                                            run.flux.s_phi,
-                                           run.flux.s_theta,
-                                           run.markers.stopped.weight,
+                                           run.flux.s_theta_2d,
+                                           run.markers.stopped.weight * run.markers.stopped.energy,
                                            h_phi_array,
                                            h_theta_2d_array,
-                                           num_grid_points=run.flux.num_grid_points)
+                                           num_grid_points=run.flux.num_grid_points_2d)
     # min_indices = np.unravel_index(np.argmin(amise_array), amise_array.shape)
     minima = minimum_filter(amise_array, size=3, mode = 'nearest') == amise_array
     min_h_coords = np.argwhere(minima)
